edf31a127d1c985fa633704d3f132cbc0273fbb7
aravappdeveloper <74691700+aravappdeveloper@users.noreply.github.com>
2022-05-17 19:33:02 -0500

    Add files via upload

---
+++ main.js
@@ -0,0 +1,55 @@
+img = "";
+status = "";
+objects = [];
+
+function preload(){
+    img = loadImage('dog_cat.jpg');
+}
+
+function setup(){
+    canvas = createCanvas(380, 380);
+    canvas.center();
+
+    video = createCapture(VIDEO);
+    video.hide();
+
+    objectDetector = ml5.objectDetector('cocossd', modelLoaded);
+    document.getElementById("status").innerHTML = "Status: Detecting Objects...";
+}
+
+function draw(){
+    image(video, 0, 0, 380, 380);
+
+    if(status != ""){
+        r = random(255);
+        g = random(255);
+        b = random(255);
+        objectDetector.detect(video, gotResult);
+        for(i = 0; i < objects.length; i++){
+            document.getElementById("status").innerHTML = "Status: Objects Detected";
+            document.getElementById("num_objects").innerHTML = "Number of objects detected: " + objects.length;
+            fill(r, g, b);
+            percent = floor(objects[i].confidence * 100);
+            text(objects[i].label + " " + percent + "%", objects[i].x, objects[i].y);
+            noFill();
+            stroke(r, g, b);
+            rect(objects[i].x, objects[i].y, objects[i].width, objects[i].height);
+        }
+    }
+}
+
+function modelLoaded(){
+    console.log("CoCoSSD Model Has Initialized!");
+    status = true;
+    objectDetector.detect(video, gotResult);
+}
+
+function gotResult(error, results){
+    if(error){
+        console.error(error);
+    }
+    else{
+        console.log(results);
+        objects = results;
+    }
+}
